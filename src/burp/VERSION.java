221af81423eefcbe47cb90971ff94952c7a16238
fi7z <dev@softwaresecured.com>
2025-05-09 12:58:58 -0400

    version bump
    (cherry picked from commit 5b266ad16d2c907c1b25c220ec19d612dcbf8973)

--- src/burp/VERSION.java
+++ src/burp/VERSION.java
@@ -3,7 +3,7 @@ package burp;
 public final class VERSION {
     public static final int VERSION_MAJOR = 1;
     public static final int VERSION_MINOR = 0;
-    public static final int VERSION_PATCH = 0;
+    public static final int VERSION_PATCH = 1;
     public static final String RELEASE_TYPE = "beta";
 
     public static String getVersionStr() {
