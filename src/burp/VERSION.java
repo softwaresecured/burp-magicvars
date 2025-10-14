2595e61a2a178492e5979ae6e08ef9ddb4e8ce5e
fi7z <dev@softwaresecured.com>
2025-10-14 15:16:09 -0400

    1.4.19

--- src/burp/VERSION.java
+++ src/burp/VERSION.java
@@ -3,8 +3,8 @@ package burp;
 public final class VERSION {
     public static final int VERSION_MAJOR = 1;
     public static final int VERSION_MINOR = 4;
-    public static final int VERSION_PATCH = 18;
-    public static final String RELEASE_TYPE = "beta";
+    public static final int VERSION_PATCH = 19;
+    public static final String RELEASE_TYPE = "release";
     public static final String RELEASE_TAGS_URL = "https://github.com/softwaresecured/burp-magicvars/tags";
     public static String getVersionStr() {
         return String.format("%d.%d.%d-%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, RELEASE_TYPE);
